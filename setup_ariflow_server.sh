99d45f92863e6d6ec6de1ac6b95f312f2ec648b7
jianhonglindst <lewis.lin1134@gmail.com>
2022-12-13 22:51:15 +0800

    Reduce the memory usage: ignore few containers

--- setup_ariflow_server.sh
+++ setup_ariflow_server.sh
@@ -13,7 +13,7 @@ if [ -z $AIRFLOW_UID ]; then sed -i "1iAIRFLOW_UID=$(id -u) " .env; fi
 # 重新 build & 啟動 Airflow Server
 # docker build -t lewis/airflow:2.3.3-python3.8 --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" .
 docker compose down --remove-orphans && \
-ssh_prv_key="$(cat ~/.ssh/id_rsa)" ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" docker compose build && \
+# ssh_prv_key="$(cat ~/.ssh/id_rsa)" ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" docker compose build && \
 # docker network create airflow-network  && \
 docker compose up airflow-init  && \
 docker compose up -d
